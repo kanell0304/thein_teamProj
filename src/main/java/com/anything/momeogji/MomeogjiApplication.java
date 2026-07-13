5519102d850dbf9e83ed3249ae610991c6d9392d
이경준 <gyeongjun@igyeongjun-ui-MacBookAir.local>
2026-07-13 17:44:55 +0900

    first commit

---
+++ src/main/java/com/anything/momeogji/MomeogjiApplication.java
@@ -0,0 +1,13 @@
+package com.anything.momeogji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomeogjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomeogjiApplication.class, args);
+	}
+
+}
